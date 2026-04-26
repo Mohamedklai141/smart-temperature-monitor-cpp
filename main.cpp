79ac42210eaee3f102c1c53d3429e9e4a28a07d2
Mohamed_klai <klaihama141@gmail.com>
2026-04-26 23:38:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 using namespace std;
 
-int main()
-{
+int main() {
     srand(time(0));
 
     int minTemp, maxTemp;
+    vector<int> readings;
 
     cout << "=== Smart Temperature Monitoring System ===" << endl;
 
@@ -17,28 +18,45 @@ int main()
     cout << "Enter maximum temperature: ";
     cin >> maxTemp;
 
-    cout << "\nMonitoring started...\n"
-         << endl;
-
-    for (int i = 1; i <= 10; i++)
-    {
+    for (int i = 1; i <= 10; i++) {
         int temp = rand() % 21 + 15;
+        readings.push_back(temp);
 
         cout << "Reading " << i << ": " << temp << " C -> ";
 
-        if (temp > maxTemp)
-        {
+        if (temp > maxTemp) {
             cout << "ALERT: Temperature too HIGH!" << endl;
         }
-        else if (temp < minTemp)
-        {
+        else if (temp < minTemp) {
             cout << "ALERT: Temperature too LOW!" << endl;
         }
-        else
-        {
+        else {
             cout << "Status: Normal" << endl;
         }
     }
+
+    int sum = 0;
+    int maxValue = readings[0];
+    int minValue = readings[0];
+
+    for (int value : readings) {
+        sum += value;
+
+        if (value > maxValue) {
+            maxValue = value;
+        }
+
+        if (value < minValue) {
+            minValue = value;
+        }
+    }
+
+    double average = (double)sum / readings.size();
+
+    cout << "\n=== Analysis ===" << endl;
+    cout << "Average temperature: " << average << " C" << endl;
+    cout << "Maximum temperature: " << maxValue << " C" << endl;
+    cout << "Minimum temperature: " << minValue << " C" << endl;
 
     cout << "\nMonitoring finished." << endl;
 
