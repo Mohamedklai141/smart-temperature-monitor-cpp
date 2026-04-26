c4fc9c8aace6fa51f73ded24bd5825b20037d394
Mohamed_klai <klaihama141@gmail.com>
2026-04-26 23:33:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,35 +3,39 @@
 #include <ctime>
 using namespace std;
 
-int main() {
+int main()
+{
     srand(time(0));
 
     int minTemp, maxTemp;
 
     cout << "=== Smart Temperature Monitoring System ===" << endl;
 
-    // User sets limits
     cout << "Enter minimum temperature: ";
     cin >> minTemp;
 
     cout << "Enter maximum temperature: ";
     cin >> maxTemp;
 
-    cout << "\nMonitoring started...\n" << endl;
+    cout << "\nMonitoring started...\n"
+         << endl;
 
-    // Generate and check values
-    for (int i = 1; i <= 10; i++) {
-        int temp = rand() % 21 + 15; // 15 to 35
+    for (int i = 1; i <= 10; i++)
+    {
+        int temp = rand() % 21 + 15;
 
         cout << "Reading " << i << ": " << temp << " C -> ";
 
-        if (temp > maxTemp) {
+        if (temp > maxTemp)
+        {
             cout << "ALERT: Temperature too HIGH!" << endl;
         }
-        else if (temp < minTemp) {
+        else if (temp < minTemp)
+        {
             cout << "ALERT: Temperature too LOW!" << endl;
         }
-        else {
+        else
+        {
             cout << "Status: Normal" << endl;
         }
     }
@@ -39,3 +43,4 @@ int main() {
     cout << "\nMonitoring finished." << endl;
 
     return 0;
+}
